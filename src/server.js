178f0ce1578bed44ca4a668eb257c8fbfa315977
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2021-08-14 13:55:05 +0500

    backend host added

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ const server = http.createServer((req,res) => {
 })
 
  
-server.listen(PORT, () => console.log('Server is running http://'+ host + ':' + PORT))
+server.listen(PORT, () => console.log('Server is running ', host + ':' + PORT))
 
 
 
